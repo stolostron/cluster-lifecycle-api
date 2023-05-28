62387789e8554a169e1adad54a6c01fb1f2da3b8
Zhiwei Yin <zyin@redhat.com>
2023-05-28 20:02:15 +0800

    upgrade api
    Signed-off-by: Zhiwei Yin <zyin@redhat.com>

--- client/action/clientset/versioned/clientset.go
+++ client/action/clientset/versioned/clientset.go
@@ -17,8 +17,7 @@ type Interface interface {
 	ActionV1beta1() actionv1beta1.ActionV1beta1Interface
 }
 
-// Clientset contains the clients for groups. Each group has exactly one
-// version included in a Clientset.
+// Clientset contains the clients for groups.
 type Clientset struct {
 	*discovery.DiscoveryClient
 	actionV1beta1 *actionv1beta1.ActionV1beta1Client
@@ -44,6 +43,10 @@ func (c *Clientset) Discovery() discovery.DiscoveryInterface {
 // where httpClient was generated with rest.HTTPClientFor(c).
 func NewForConfig(c *rest.Config) (*Clientset, error) {
 	configShallowCopy := *c
+
+	if configShallowCopy.UserAgent == "" {
+		configShallowCopy.UserAgent = rest.DefaultKubernetesUserAgent()
+	}
 
 	// share the transport between all clients
 	httpClient, err := rest.HTTPClientFor(&configShallowCopy)
--- client/imageregistry/clientset/versioned/clientset.go
+++ client/imageregistry/clientset/versioned/clientset.go
@@ -17,8 +17,7 @@ type Interface interface {
 	ImageregistryV1alpha1() imageregistryv1alpha1.ImageregistryV1alpha1Interface
 }
 
-// Clientset contains the clients for groups. Each group has exactly one
-// version included in a Clientset.
+// Clientset contains the clients for groups.
 type Clientset struct {
 	*discovery.DiscoveryClient
 	imageregistryV1alpha1 *imageregistryv1alpha1.ImageregistryV1alpha1Client
@@ -44,6 +43,10 @@ func (c *Clientset) Discovery() discovery.DiscoveryInterface {
 // where httpClient was generated with rest.HTTPClientFor(c).
 func NewForConfig(c *rest.Config) (*Clientset, error) {
 	configShallowCopy := *c
+
+	if configShallowCopy.UserAgent == "" {
+		configShallowCopy.UserAgent = rest.DefaultKubernetesUserAgent()
+	}
 
 	// share the transport between all clients
 	httpClient, err := rest.HTTPClientFor(&configShallowCopy)
